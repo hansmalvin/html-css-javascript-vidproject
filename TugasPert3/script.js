a7aeba5de4b6546dbfe50c827fec070144177a66
hansmalvin <hansmalvin7@gmail.com>
2024-09-08 22:50:51 +0700

    Tugas Pertemuan 3

---
+++ TugasPert3/script.js
@@ -0,0 +1,17 @@
+let inputX = document.getElementById('varA');
+let inputY = document.getElementById('varB');
+let out = document.getElementById('output');
+
+document.getElementById("tambah").addEventListener("click", function (){
+    out.value = parseInt(inputX.value)+parseInt(inputY.value);
+});
+document.getElementById("kurang").addEventListener("click", function (){
+    out.value = parseInt(inputX.value)-parseInt(inputY.value);
+});
+document.getElementById("kali").addEventListener("click", function (){
+    out.value = parseInt(inputX.value)*parseInt(inputY.value);
+});
+document.getElementById("bagi").addEventListener("click", function (){
+    out.value = parseInt(inputX.value)/parseInt(inputY.value);
+});
+//gunakan jenis fungsi yang berbeda beda untuk tujuan pembelajaran
